d053429a69aba6ce1a35533ff4e2f86b0c162dc0
relaxday123 <91047548+relaxday123@users.noreply.github.com>
2022-05-18 16:49:42 +0700

    fixed

--- app/src/main/java/com/example/music_player/Fragment/Banner.java
+++ app/src/main/java/com/example/music_player/Fragment/Banner.java
@@ -66,11 +66,7 @@ public class Banner extends Fragment {
                     public void run() {
                         curItem = viewPager.getCurrentItem();
                         curItem++;
-<<<<<<< Updated upstream
-                        if(curItem >= viewPager.getAdapter().getCount()) {
-=======
                         if(curItem >= 3) { //viewPager.getAdapter().getCount()
->>>>>>> Stashed changes
                             curItem = 0;
                         }
                         viewPager.setCurrentItem(curItem,true);
